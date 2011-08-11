c3eec752e1bebe3fd59f9621b3a5ffb099083a21
John Dyer <john@krumpt.com>
2011-08-11 13:18:39 -0500

    changed token api param name

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -8,7 +8,7 @@ module Helpers
   
   def send_sms(opts={})
       puts "Sending msg [ #{CGI.escape(opts[:msg])} ] => #{opts[:number_to_msg]}"
-      response = RestClient.get "http://api.tropo.com/1.0/sessions?action=create&token=#{TOKEN_ID}&user_number=#{opts[:number_to_msg]}&msg=#{CGI.escape(opts[:msg])}"
+      response = RestClient.get "http://api.tropo.com/1.0/sessions?action=create&token=#{TOKEN_ID}&number_to_msg=#{opts[:number_to_msg]}&msg=#{CGI.escape(opts[:msg])}"
       if response.eql?(200)
         puts "Msg sent HTTP/#{response.code}"
         true
